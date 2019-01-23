c1f494bc5544a5c10f4ea9cdb63763371e53ff88
Wallace Benevides <wallacebenevides@hotmail.com>
2019-01-23 14:04:06 -0200

    Fix aplicando filtros ao evento change

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 //const json = require('./noticias.json');
 import '../assets/JSON/noticias.json';
-import './style';//carrega os arquivos de olha de estilo
+import './style';//carrega os arquivos de folha de estilo
 import { currentInstance } from "./controllers/EditoriaController";
 var editoriaController = currentInstance();
 
 var oderna = editoriaController.ordena.bind(editoriaController);
 var filtra = editoriaController.filtra.bind(editoriaController);
 
-document.querySelector("#ordenar").onclick = oderna;
-document.querySelector("#filtrar").onclick = filtra;
+document.querySelector("#ordenar").onchange = oderna;
+document.querySelector("#filtrar").onchange = filtra;
